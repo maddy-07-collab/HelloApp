ca0c96e38e3debd8a7f255fcc951281b5df457a4
rv9198@srmist.edu.in <rv9198@srmist.edu.in>
2026-04-20 13:18:19 +0530

    Displaying Hello World with Default Arg

--- src/main/HelloApp.java
+++ src/main/HelloApp.java
@@ -1,10 +1,24 @@
 public class HelloApp{
-            public static void main (String[] args){
-			         String name = "World";
-					 if(args.length>0){
-						 name=args[0];
-					 }	 
-					 System.out.println("Hello"+name+"!");
-			}		 
-}		
+    public static void main(String[] args) {
+        String name;
+        if (args.length > 0) {
+            StringBuilder nameBuilder = new StringBuilder();
 			
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+
+            name = nameBuilder.toString();
+        } else {
+            // Default value if no arguments
+            name = "World";
+        }
+
+        // Print the greeting
+        System.out.println("Hello " + name);
+    }
+}
